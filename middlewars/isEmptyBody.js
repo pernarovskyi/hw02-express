5090375f9e7b81ead77c3fc10078f36531f57326
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2023-07-24 22:02:47 +0300

    small fix

--- middlewars/isEmptyBody.js
+++ middlewars/isEmptyBody.js
@@ -4,7 +4,7 @@ const isEmptyBody = (req, res, next) => {
   const { length } = Object.keys(req.body);
 
   if (!length) {
-    next(HttpError(400, "Body require filds"));
+    next(HttpError(400, "Missing required fields"));
   }
   next();
 };
