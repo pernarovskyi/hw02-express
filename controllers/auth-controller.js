70a5e42e581abcd99585860b029ba0645007cd51
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2023-07-30 21:24:04 +0300

    added password hashing

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,16 +1,19 @@
 const User = require("../models/user.js");
 const { HttpError } = require("../helpers/index.js");
 const { ctrlWrapper } = require("../decorators/index.js");
+const { hash, compare } = require("bcryptjs");
 
 
 const signUp = async (req, res) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({email});
 
     if(user) {
         throw HttpError(409, "Email already in use.");
     }
-    const newUser = await User.create(req.body);
+
+    const hashPassword = await hash(password, 10);
+    const newUser = await User.create({...req.body, password: hashPassword});
 
     res.status(201).json({
         name: newUser.name,
